416f3ddd390de93872f1fd3af0c49a41e92afa8e
ScottHarrisonDev <github@scottharrisondesign.co.uk>
2021-07-03 00:26:27 +0100

    add request format check

--- server/controllers/planets.controller.ts
+++ server/controllers/planets.controller.ts
@@ -7,6 +7,10 @@ export const planetsRouter = express.Router();
 
 planetsRouter.get("/celestial-bodies", async (req: Request<{},{},{},celestialBodyReq>, res: Response) => {
   try {
+    if (req.query.page === undefined || req.query.perPage === undefined) {
+      res.status(400).send("Please match the required request format.");
+      return;
+    }
     const planets: Planet[] = await PlanetService.findByPage(parseInt(req.query.page), parseInt(req.query.perPage));
 
     res.status(200).send(planets);
