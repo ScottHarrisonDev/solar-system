bf2948c3605c37332929b056b762492bd94fc064
ScottHarrisonDev <github@scottharrisondesign.co.uk>
2021-07-03 13:42:39 +0100

    add error handling and remove any type

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,22 +2,35 @@ import React, {useState} from 'react';
 import './App.scss';
 import {Header} from "./components/Header/Header";
 import {Planet} from "./components/Planet/Planet";
+import {ErrorMessage} from "./components/ErrorMessage/ErrorMessage";
+import {PlanetInterface} from "./Interfaces/PlanetInterface";
 
 function App() {
-  const [planets, setPlanets] = useState<any[]>([])
+  const [error, setError] = useState<string | null>(null);
+  const [planets, setPlanets] = useState<PlanetInterface[]>([]);
 
   React.useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/celestial-bodies`)
-        .then((res) => res.json())
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/celestial-bodies?page=1&perPage=10`)
+        .then((res) => {
+          if (! res.ok) {
+            res.text().then(error => {
+              setError(`API Error ${error}`);
+            });
+          }
+          return res.json();
+        })
         .then((data) => setPlanets(data))
-        .catch(error => {
-          console.error('There was an unexpected error: ', error);
+        .catch(() => {
+          setError("There was an unexpected error");
         });
   }, []);
 
   return (
       <div className="App">
         <Header title="The Solar System"/>
+        {error !== null &&
+          <ErrorMessage message={error} />
+        }
         <div className={"planet-wrapper"}>
           {planets.map(planet => (
               <Planet
